a682153985f451a6f9f3a3247bd49bb5d74d4769
saray <saragaby314@gmail.com>
2026-03-31 20:47:00 +0200

    feat: crear ApiService para obtener personajes de Sailor Moon desde la API

---
+++ services/ApiService.js
@@ -0,0 +1,22 @@
+class ApiService {
+
+  static BASE_URL = "https://api.jikan.moe/v4";
+
+  static async getCharacters() {
+
+    const endpoint = "/anime/23205/characters"; 
+    
+    const url = `${this.BASE_URL}${endpoint}`;
+
+    const response = await fetch(url);
+
+    if (!response.ok) {
+
+      throw new Error(`Error al obtener personajes: ${response.status}`);
+    }
+
+    const json = await response.json();
+
+    return json.data; 
+  }
+}
